727293c83c81c9cf3388c8035de2fb094fb062d9
SangWoo <sangw0804@likelion.org>
2019-04-05 01:29:26 +0900

    cors & corb 해결

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+const config = require('./config');
 
 const app = express();
 
@@ -8,14 +10,20 @@ app.use(cors({ origin: true }));
 app.get('/movie_search', async (req, res) => {
   try {
     const { movie } = req.query;
-    const response = await fetch(
-      `https://openapi.naver.com/v1/search/movie.json?query=${movie}`
+    const { data } = await axios(
+      `https://openapi.naver.com/v1/search/movie.json?query=${encodeURI(
+        movie
+      )}`,
+      {
+        headers: {
+          'X-Naver-Client-Id': config.NAVER_API.CLIENT_ID,
+          'X-Naver-Client-Secret': config.NAVER_API.CLIENT_SECRET
+        }
+      }
     );
 
-    const data = await response.json();
-
-    console.log(data);
-    res.send({ data: 1 });
+    console.log(data.items);
+    res.send(JSON.stringify(data.items));
   } catch (e) {
     console.log(e);
   }
